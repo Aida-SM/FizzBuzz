7d93ee5215b8f1de6ee84247260dd2e11e0e21e1
Aida Sabaghmoghadam <aida.sabbagh@gmail.com>
2019-02-16 18:01:16 -0500

    creating actual Buzz in Main.class

--- src/com/exercises/TDD/java/Main.java
+++ src/com/exercises/TDD/java/Main.java
@@ -4,9 +4,13 @@ public class Main {
     public static void main(String[] args) {
 
         Fizz fizz = new Fizz();
+        Buzz buzz = new Buzz();
         for ( int givenNumber = 1; givenNumber<=100; givenNumber++) {
             if (givenNumber % 3 == 0)
                 System.out.println(fizz.getNumber(givenNumber));
+            else if (givenNumber%5==0)
+                System.out.println(buzz.getNumber(givenNumber));
+
 
         }
 
