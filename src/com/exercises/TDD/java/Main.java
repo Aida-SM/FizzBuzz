c21cd87119b5889c416f8d9c1a18091cbdb9977c
Aida Sabaghmoghadam <aida.sabbagh@gmail.com>
2019-02-16 17:56:41 -0500

    creating actual Fizz in Main.class

--- src/com/exercises/TDD/java/Main.java
+++ src/com/exercises/TDD/java/Main.java
@@ -2,7 +2,18 @@ public class Main {
 
 
     public static void main(String[] args) {
+
+        Fizz fizz = new Fizz();
+        for ( int givenNumber = 1; givenNumber<=100; givenNumber++) {
+            if (givenNumber % 3 == 0)
+                System.out.println(fizz.getNumber(givenNumber));
+
+        }
+
+
+
     }
+
 
 
 
